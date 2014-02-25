6e7a489bc704aafa125e46d80c06ab8b89a26a8b
Mike MacHenry <mike.machenry@gmail.com>
2014-02-25 00:54:19 -0500

    adding some failing tests

--- test/eval_test.ml
+++ test/eval_test.ml
@@ -1,17 +1,25 @@
 open OUnit
 
-let check_eval program value = fun () ->
-    assert_equal (Eval.eval (Read.ast_of_string program))
-                 (Read.ast_of_string value)
+let eval_string (program : string) : string =
+    Ast.string_of_ast (Eval.eval (Read.ast_of_string program))
+
+let check_eval p1 p2 () = assert_equal (eval_string p1) (eval_string p2)
 
 let tests = "Test eval" >::: [
     "Number" >:: (check_eval "413" "413");
-    "Func app" >:: (check_eval "413"
+    "App" >:: (check_eval "(fn x => x) 413" "413");
+    "Higher-order function" >:: (check_eval "413"
         "(fn x => x 412) succ");
     "If" >:: (check_eval "413"
         "if iszero(pred (pred 2)) then succ (pred 413) else 2");
     "two params" >:: (check_eval "2"
-        "(fn m => (fn n => if iszero n then n else n)) 0 2")
+        "(fn m => (fn n => if iszero n then n else n)) 0 2");
+    "Recursive binary add" >:: (check_eval
+        "(rec add => fn n => fn m =>
+            if iszero n
+            then m
+            else succ(add (pred n) m)) 400 13"
+        "413")
     ]
 (*
 "(fn mul =>
--- test/read_test.ml
+++ test/read_test.ml
@@ -1,28 +1,54 @@
 open OUnit
-(*open Pcf*)
+open Ast
 
+let check_read str ast () = assert_equal (Read.ast_of_string str) ast
 
 let tests = "Test parser" >::: [
-    "Simple ID" >::
-    (fun () -> (assert_equal (Ast.Id "x") (Read.ast_of_string "x")));
+    "Simple ID" >:: check_read "x" (Id "x");
 
-    "Application of simple ID" >::
-    (fun () -> (assert_equal (Read.ast_of_string "x x")
-        (Ast.App ((Ast.Id "x"), (Ast.Id "x")))));
+    "Application of simple ID" >:: check_read
+        "x x"
+        (App ((Id "x"), (Id "x")));
 
-    "Identity function" >::
-    (fun () -> (assert_equal (Read.ast_of_string "fn param => param")
-        (Ast.Fun ("param", (Ast.Id "param")))));
+    "Identity function" >:: check_read
+        "fn param => param"
+        (Fun ("param", (Id "param")));
+
+    "Tokens on two lines stay separate" >:: check_read
+        "(fn x => fn y => y)
+        2
+        413"
+        (App (App ((Fun ("x", Fun ("y", Id "y"))),
+                  Int 2),
+             Int 413));
+        
+    "Recursive binary add" >:: check_read
+        "(rec add => fn n => fn m =>
+            if iszero n
+            then m
+            else succ(add (pred n) m))
+        400
+        13"
+        (App (Rec ("add", (Fun ("n", Fun ("m",
+             (If (App (Id "iszero",Id "n"),
+                 Id "m",
+                 (App (Id "succ",
+                      (App ((App (Id "add",
+                                 (App (Id "pred", Id "n")))),
+                           Id "m")))))))))),
+             Int 40013));
 
     "Factorial" >::
     (fun () -> (assert_equal
-        (Read.ast_of_string "rec fact => fn n => if iszero n then 1 else mul n (fact (pred n))")
-        (Ast.Rec ("fact",
-            (Ast.Fun ("n",
-                (Ast.If ((Ast.App (Ast.Id "iszero", (Ast.Id "n"))),
-                         (Ast.Int 1),
-                         (Ast.App ((Ast.App ((Ast.Id "mul"),
-                                             (Ast.Id "n"))),
-                                  (Ast.App ((Ast.Id "fact"),
-                                            (Ast.App (Ast.Id "pred", (Ast.Id "n")))))))))))))))
+        (Read.ast_of_string
+            "rec fact => fn n => if iszero n then 1 else mul n (fact (pred n))")
+        (Rec ("fact",
+            (Fun ("n",
+                (If ((App (Id "iszero", (Id "n"))),
+                         (Int 1),
+                         (App ((App ((Id "mul"),
+                                             (Id "n"))),
+                                  (App ((Id "fact"),
+                                            (App (Id "pred", (Id "n"))))))))
+                )))))))
     ]
