16355a1d99565690799c22ec7baf79eb67bade3f
Mike MacHenry <mike.machenry@gmail.com>
2014-02-25 00:59:38 -0500

    fixed incorrect usage detection

--- src/main.ml
+++ src/main.ml
@@ -12,7 +12,7 @@ let read_file (filename : string) : string =
 
 let () =
     Arg.parse (Arg.align speclist) (fun str -> filename := str) usage_msg;
-    if !filename == "" (* not working *)
+    if !filename = ""
     then print_endline usage_msg
     else let str = read_file !filename in
          let ast = Read.ast_of_string str in
